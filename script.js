ee59709635bad625d37bafce7275f51922e7fc7b
monu-kumar-dev <monukumar.software@gmail.com>
2026-07-07 17:06:51 +0530

    feat: persist and render todos from localStorage

--- script.js
+++ script.js
@@ -12,20 +12,20 @@ const completedBtn = document.getElementById("completedBtn");
 const pendingBtn = document.getElementById("pendingBtn");
 const allBtn = document.getElementById("allBtn");
 
-function addTodo() {
-  if (todoInput.value === "" || todoInput.value.trim() === "") {
-    errorMsg.innerText = "Please enter a task";
-    return;
-  }
+const todos = JSON.parse(localStorage.getItem("todoTask")) || [];
 
+function createTodoElement(todo) {
   const taskItem = document.createElement("div");
   const taskText = document.createElement("p");
   taskItem.classList.add("task-item");
   taskText.classList.add("task-text");
 
-  taskText.innerText = todoInput.value.trim();
+  taskText.innerText = todo.text;
 
-  // taskItem.appendChild(taskText);
+  // Agar task completed hai to class add karo
+  if (todo.completed) {
+    taskItem.classList.add("task-completed");
+  }
 
   //   Adding button
   const taskActions = document.createElement("div");
@@ -50,11 +50,6 @@ function addTodo() {
   taskItem.append(taskText, taskActions);
 
   todoList.appendChild(taskItem);
-
-  updateCounters();
-
-  // clear inputBtn
-  todoInput.value = "";
 
   // Handling complete btn
   completeBtn.addEventListener("click", () => {
@@ -91,6 +86,100 @@ function addTodo() {
       updateCounters();
     }
   });
+}
+
+function addTodo() {
+  if (todoInput.value === "" || todoInput.value.trim() === "") {
+    errorMsg.innerText = "Please enter a task";
+    return;
+  }
+
+  const todo = {
+    text: todoInput.value.trim(),
+    completed: false,
+  };
+
+  todos.push(todo);
+  // console.log(todos);
+  localStorage.setItem("todoTask", JSON.stringify(todos));
+
+  createTodoElement(todo);
+  updateCounters();
+  todoInput.value = "";
+
+  // const taskItem = document.createElement("div");
+  // const taskText = document.createElement("p");
+  // taskItem.classList.add("task-item");
+  // taskText.classList.add("task-text");
+
+  // taskText.innerText = todoInput.value.trim();
+
+  // // taskItem.appendChild(taskText);
+
+  // //   Adding button
+  // const taskActions = document.createElement("div");
+  // taskActions.classList.add("task-actions");
+
+  // const completeBtn = document.createElement("button");
+  // completeBtn.type = "button";
+  // completeBtn.classList.add("complete-btn");
+  // completeBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+  // const editBtn = document.createElement("button");
+  // editBtn.type = "button";
+  // editBtn.classList.add("edit-btn");
+  // editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+
+  // const deleteBtn = document.createElement("button");
+  // deleteBtn.type = "button";
+  // deleteBtn.classList.add("delete-btn");
+  // deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+  // taskActions.append(completeBtn, editBtn, deleteBtn);
+  // taskItem.append(taskText, taskActions);
+
+  // todoList.appendChild(taskItem);
+
+  // updateCounters();
+
+  // // clear inputBtn
+  // todoInput.value = "";
+
+  // // Handling complete btn
+  // completeBtn.addEventListener("click", () => {
+  //   // taskItem.classList.add("task-completed");
+
+  //   // if (taskItem.classList.contains("task-completed")) {
+  //   //   taskItem.classList.remove("task-completed");
+  //   // } else {
+  //   //   taskItem.classList.add("task-completed");
+  //   // }
+
+  //   // In-short
+  //   taskItem.classList.toggle("task-completed");
+  //   updateCounters();
+  // });
+
+  // // Handling edit btn
+  // editBtn.addEventListener("click", () => {
+  //   todoInput.value = taskText.innerText;
+  //   todoInput.focus();
+  //   taskItem.remove();
+
+  //   updateCounters();
+  // });
+
+  // // Handling delete btn
+  // deleteBtn.addEventListener("click", () => {
+  //   // User se confirmation mangne ke liye
+  //   const isConfirmed = confirm("Are you sure you want to delete this task?");
+
+  //   // Agar user "OK" click karta hai, to task delete ho jayega
+  //   if (isConfirmed) {
+  //     taskItem.remove();
+  //     updateCounters();
+  //   }
+  // });
 }
 
 function updateCounters() {
@@ -151,8 +240,17 @@ completedBtn.addEventListener("click", () => {
   });
 });
 
+
+
 // addBtn.addEventListener("submit", addTodo);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   addTodo();
 });
+
+// Initial Load
+todos.forEach((todo) => {
+  createTodoElement(todo);
+});
+
+updateCounters();
